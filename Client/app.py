b19b9afe06021d745f1f193c8bb46123d4644aee
babyNEKO <34335425+babyNEKO@users.noreply.github.com>
2021-10-07 21:10:58 +0800

    新增文件上传功能，前端新增上传界面
    删除冗余代码，优化结构。
    2021年10月7日

--- Client/app.py
+++ Client/app.py
@@ -1,15 +1,36 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from ESM_Plugin.HostStatus import cpu_info, memory_info, disk_info, system_info, network_info, services_info
+from os import listdir, mkdir
+from os.path import exists, join as os_path_join
+from werkzeug.utils import secure_filename
 
-upload_folder = ''
+upload_folder = 'c:\\ESM_Client_Upload\\'
 allowed_extensions = {'zip', 'rar', '7z', 'tar'}
 
 ESM_Client = Flask(__name__)
 ESM_Client.config['UPLOAD_FOLDER'] = upload_folder
 
 
-@ESM_Client.route('/')
+def check_upload_folder():
+    if upload_folder == '':
+        return {'info': 'Not Set Upload Folder!', 'lv': 0}
+    if upload_folder != '':
+        if not exists(upload_folder):
+            mkdir(upload_folder)
+        if len(listdir(upload_folder)) == 0:
+            return {'info': 'Without any files.', 'lv': 1}
+        else:
+            return {'info': listdir(upload_folder), 'lv': 2}
+
+
+@ESM_Client.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        upload_file = request.files['file']
+        if upload_file.filename != '':
+            upload_file.save(os_path_join(upload_folder, secure_filename(upload_file.filename)))
+        return redirect(url_for('index'))
+
     return render_template(
         'index.html',
         cpu_info=cpu_info(),
@@ -18,6 +39,7 @@ def index():
         system_info=system_info(),
         network_info=network_info(),
         services_info=services_info(),
+        check_upload=check_upload_folder(),
     )
 
 
@@ -35,11 +57,7 @@ def get_all_info():
     return jsonify(all_info)
 
 
-@ESM_Client.route('/receive_file/', methods=['POST'])
-def receive_file():
-    pass
-
-
 if __name__ == '__main__':
+    # test server, all IP address access, port 5000
     ESM_Client.debug = True
     ESM_Client.run(host='0.0.0.0')
